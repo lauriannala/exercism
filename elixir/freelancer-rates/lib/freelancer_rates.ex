8d357120b39ecc3838d250ffd0d330b867bbc29d
Lauri Annala <lauri.annala@protonmail.com>
2022-06-04 14:45:51 +0300

    Freelancer rates

--- elixir/freelancer-rates/lib/freelancer_rates.ex
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -1,17 +1,16 @@
 defmodule FreelancerRates do
-  def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
-  end
+  def daily_rate(hourly_rate), do: 8.0 * hourly_rate
 
-  def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
-  end
+  def apply_discount(before_discount, discount), do: before_discount - discount * before_discount * 0.01
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    daily = daily_rate(hourly_rate)
+    monthly = daily * 22
+    apply_discount(monthly, discount) |> :math.ceil() |> trunc()
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    daily = daily_rate(hourly_rate) |> apply_discount(discount)
+    (budget / daily) |> Float.floor(1)
   end
 end
