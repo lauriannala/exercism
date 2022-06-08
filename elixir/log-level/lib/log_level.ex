adf2af200bd141ac2645825f6d1b2ac9a6ff69c5
Lauri Annala <lauri.annala@erlang-solutions.com>
2022-06-08 10:19:36 +0300

    Log level with cond

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -1,19 +1,25 @@
 defmodule LogLevel do
-  def to_label(_level = 0, _legacy? = false), do: :trace
-  def to_label(_level = 1, _legacy?), do: :debug
-  def to_label(_level = 2, _legacy?), do: :info
-  def to_label(_level = 3, _legacy?), do: :warning
-  def to_label(_level = 4, _legacy?), do: :error
-  def to_label(_level = 5, _legacy? = false), do: :fatal
-  def to_label(_level, _legacy?), do: :unknown
+  def to_label(level, legacy?) do
+    cond do
+      level == 0 and legacy? == false -> :trace
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      level == 5 and legacy? == false -> :fatal
+      true -> :unknown
+    end
+  end
 
   def alert_recipient(level, legacy?) do
-    case to_label(level, legacy?) do
-      :error -> :ops
-      :fatal -> :ops
-      :unknown when legacy? == true -> :dev1
-      :unknown when legacy? == false -> :dev2
-      _ -> false
+    label = to_label(level, legacy?)
+
+    cond do
+      label == :error -> :ops
+      label == :fatal -> :ops
+      label == :unknown and legacy? == true -> :dev1
+      label == :unknown and legacy? == false -> :dev2
+      true -> false
     end
   end
 end
