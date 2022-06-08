e74365cdb64f023742eef3cccc450b99260ef585
Lauri Annala <lauri.annala@erlang-solutions.com>
2022-06-08 10:11:25 +0300

    Log level with function heads

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -1,9 +1,19 @@
 defmodule LogLevel do
-  def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
-  end
+  def to_label(_level = 0, _legacy? = false), do: :trace
+  def to_label(_level = 1, _legacy?), do: :debug
+  def to_label(_level = 2, _legacy?), do: :info
+  def to_label(_level = 3, _legacy?), do: :warning
+  def to_label(_level = 4, _legacy?), do: :error
+  def to_label(_level = 5, _legacy? = false), do: :fatal
+  def to_label(_level, _legacy?), do: :unknown
 
   def alert_recipient(level, legacy?) do
-    # Please implement the alert_recipient/2 function
+    case to_label(level, legacy?) do
+      :error -> :ops
+      :fatal -> :ops
+      :unknown when legacy? == true -> :dev1
+      :unknown when legacy? == false -> :dev2
+      _ -> false
+    end
   end
 end
