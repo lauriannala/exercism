9d617c62d2dccac0074c03792053c8a7435e6d2b
Lauri Annala <lauri.annala@protonmail.com>
2022-06-12 20:23:16 +0300

    File sniffer

---
+++ elixir/file-sniffer/lib/file_sniffer.ex
@@ -0,0 +1,22 @@
+defmodule FileSniffer do
+  def type_from_extension("exe"), do: "application/octet-stream"
+  def type_from_extension("bmp"), do: "image/bmp"
+  def type_from_extension("png"), do: "image/png"
+  def type_from_extension("jpg"), do: "image/jpg"
+  def type_from_extension("gif"), do: "image/gif"
+
+  def type_from_binary(<<0x7F, 0x45, 0x4C, 0x46, _::binary>>), do: type_from_extension("exe")
+  def type_from_binary(<<0x42, 0x4D, _::binary>>), do: type_from_extension("bmp")
+  def type_from_binary(<<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _::binary>>), do: type_from_extension("png")
+  def type_from_binary(<<0xFF, 0xD8, 0xFF, _::binary>>), do: type_from_extension("jpg")
+  def type_from_binary(<<0x47, 0x49, 0x46, _::binary>>), do: type_from_extension("gif")
+
+  def verify(file_binary, extension) do
+    extension_result = type_from_extension(extension)
+
+    case extension_result == type_from_binary(file_binary) do
+      true -> {:ok, extension_result}
+      false -> {:error, "Warning, file format and file extension do not match."}
+    end
+  end
+end
