4c5f5e7fce3e12da3621bfcd613ddf35410baf70
Lauri Annala <lauri.annala@erlang-solutions.com>
2022-06-13 11:09:32 +0300

    Rpg

---
+++ elixir/bread-and-potions/lib/rpg.ex
@@ -0,0 +1,45 @@
+defmodule RPG do
+  defmodule Character do
+    defstruct health: 100, mana: 0
+  end
+
+  defmodule LoafOfBread do
+    defstruct []
+  end
+
+  defmodule ManaPotion do
+    defstruct strength: 10
+  end
+
+  defmodule Poison do
+    defstruct []
+  end
+
+  defmodule EmptyBottle do
+    defstruct []
+  end
+
+  defprotocol Edible do
+    @spec eat(item :: map(), character :: map()) ::
+            {by_product :: map() | nil, character :: map()}
+    def eat(item, character)
+  end
+
+  defimpl Edible, for: LoafOfBread do
+    def eat(_, %Character{health: health} = character) do
+      {nil, %Character{character | health: health + 5}}
+    end
+  end
+
+  defimpl Edible, for: ManaPotion do
+    def eat(%{strength: strength}, %Character{mana: mana} = character) do
+      {%EmptyBottle{}, %Character{character | mana: mana + strength}}
+    end
+  end
+
+  defimpl Edible, for: Poison do
+    def eat(_, %Character{} = character) do
+      {%EmptyBottle{}, %Character{character | health: 0}}
+    end
+  end
+end
